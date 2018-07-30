1d7a6ff2fa99ca474fc2139eb123a85044c9c978
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-07-30 17:45:25 +0100

    Jo's solution

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.characters.collect {|characters| "#{characters.name} - #{characters.show.name}"}
+  end
+
+
 end
